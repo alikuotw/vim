756e2b90dbb8d220c3a27e4a5947c59acf089501
alikuotw <alikuotw.guo@estinet.com>
2018-05-30 15:09:56 +0800

    update print_format

--- scripts/ali_print_format.sh
+++ scripts/ali_print_format.sh
@@ -1,78 +1,24 @@
 #!/bin/bash
 
-# add this lina to the top of file can be used. "source ali_print_format.sh"
-# format function is reference http://man.linuxde.net/tput
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+white="\033[1;37m"
+reset="\033[0m"
+tab="\t\t"
 
-# tput setb
-
-#   [1-7] Set background color
-# tput setab
-
-#   [1-7] Set ANSI escape code background color
-# tput setf
-
-#   [1-7] Set font color
-# tput setaf
-
-#   [1-7] Set ANSI escape font color
-# tput blod
-
-# tput smul
-#   underline
-
-# tput rev
-
-# tput sgr0
-#   exit.
-
-### printFormat function ###
-# $1 output string
-# $2 color 0-7
-# $3 bgcolor 0-7
-# $4 bold 0-1
-# $5 underline 0-1
-
-function printFormat()
-{
-    str=$1
-    color=$2
-    bgcolor=$3
-    bold=$4
-    underline=$5
-
-    reset=$(tput sgr0;)
-    setcolor=
-    setbgcolor=
-    setbold=
-    setunderline=
-
-    case "$color" in
-        0|1|2|3|4|5|6|7)
-            setcolor=$(tput setaf $color;)
-            ;;
-        *)
-            ;;
-    esac
-
-    case "$bgcolor" in
-        0|1|2|3|4|5|6|7)
-            setcolor=$(tput setab $bgcolor;)
-            ;;
-        *)
-            ;;
-    esac
-
-    if [ "$bold" == "1" ]; then
-        setbold=$(tput bold;)
-    fi
-
-    if [ "$underline" == "1" ]; then
-        setunderline=$(tput smul;)
-    fi
-
-    echo "${setcolor}${setbgcolor}${setbold}${setunderline}${str}${reset}"
+print_info() {
+    echo "${yellow}$1${reset}: $tab ${white}$2${reset}"
 }
 
-function printError() { printFormat "$1" 1 -1 1; }
-function printInfo() { printFormat "$1" 2; }
-function printText() { printFormat "$1"; }
+print_text() {
+    echo "$white$1$reset"
+}
+
+print_error() {
+    echo "$red$1$reset"
+}
+
+print_tips() {
+    echo "$green$1$reset"
+}
